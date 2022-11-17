95cb528d8fd681d3a591cbcdcb5b1e939b008215
Leonardo Campos <0000759006@senaimgaluno.com.br>
2022-11-16 21:48:27 -0300

    Essa versão inclui a sexta e o domingo

---
+++ diasemana3.py
@@ -0,0 +1,12 @@
+﻿print("Qual dia da semana é hoje ?")
+diasemana = input()
+if diasemana == "sabado":
+    print("Hoje é dia de feijoada e uma cerveja!")
+else:
+    if diasemana == "sexta":
+        print("Bora tomar uma")
+    else:
+        if diasemana == "domingo":
+            print("Hoje é dia de ir na missa")
+        else:
+            print("Hoje não é dia de gastação")
